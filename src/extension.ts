4dd9147a91dad6f8a3a73428940dde26bcdb736a
Samir Rashid <samirsgotmail@gmail.com>
2023-11-04 18:31:00 -0700

    annotation: Annotate every line of file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { annotateFile, annotateLine } from "./annotation";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,13 +13,22 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("pytrail.helloWorld", () => {
+  let disposable = vscode.commands.registerCommand("pytrail.version", () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from pytrail!");
+    vscode.window.showInformationMessage("pytrail2");
   });
 
+  let doAnnotating = vscode.commands.registerCommand(
+    "pytrail.doAnnotating",
+    () => {
+      // annotateLine(lineNum);
+      annotateFile();
+    },
+  );
+
   context.subscriptions.push(disposable);
+  context.subscriptions.push(doAnnotating);
 }
 
 // This method is called when your extension is deactivated
