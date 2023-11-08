450ab07abf42fb6fe71ba4ae6aa7b4844a4d350c
amber3536 <36279682+amber3536@users.noreply.github.com>
2023-11-08 09:46:35 -0800

    Add files via upload

---
+++ tasks/task2.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+def foo():
+    sleep(1.01)
+    return 3
+
+def main():
+    arr = list(range(250000))
+    tmp = []
+    cnt = foo()
+
+    for row in arr:
+        if row % cnt == 0:
+            tmp.append(row)
+    result = (sum(tmp * 200) + len(arr)) / len(tmp)  # comment action #3
+    return result
+
