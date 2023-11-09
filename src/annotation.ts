6a9ff7580da46da41aff1103dff2195f0516cd05
Samir Rashid <samirsgotmail@gmail.com>
2023-11-09 02:09:22 -0800

    feat: add multi-file annotating

--- src/annotation.ts
+++ src/annotation.ts
@@ -1,6 +1,63 @@
+import { TextDocument } from "vscode";
+import {
+  DEBUG,
+  myOutputChannel,
+  annotationData,
+  decorationType,
+} from "./constants";
 const vscode = require("vscode");
 
-// may need to make this async if it takes a while?
 export function annotateLine(lineNum: number) {}
 
-export function annotateFile() {}
+export async function annotateFile(document: TextDocument | undefined) {
+  if (document) {
+    if (annotationData.size == 0) {
+      console.log(
+        "annotation: There is no annotation data to use! Try running `pytrail: Get Trace Data` in the command palette.",
+      );
+      vscode.window.showErrorMessage(
+        "annotation: There is no annotation data to use! Try running `pytrail: Get Trace Data` in the command palette.",
+      );
+      return;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+    const activeEditor = vscode.window.activeTextEditor;
+
+    if (activeEditor) {
+      const filePath = activeEditor.document.fileName;
+      const fileAnnotationData = annotationData.get(filePath);
+      console.log(`current editor is ${filePath}`);
+
+      const decorations = [];
+
+      for (const line in fileAnnotationData) {
+        const lineData = fileAnnotationData[line];
+
+        // TODO: format floats better and need to add coloring logic
+        const time = lineData.n_sys_percent.toFixed(2).padStart(5, "0"); // TODO: need to read details on the different profiling statistics
+        const lineNumber = lineData.lineno - 1; // VSCode lines are 0 indexed
+
+        myOutputChannel.appendLine(
+          "line " + lineNumber + " took " + time + "percent to run",
+        );
+        if (lineNumber >= 0 && lineNumber < editor.document.lineCount) {
+          myOutputChannel.appendLine(" ↳ ADDED");
+
+          const range = editor.document.lineAt(lineNumber).range;
+          const decoration = {
+            range,
+            renderOptions: {
+              before: {
+                contentText: `${time}% `,
+              },
+            },
+          };
+
+          decorations.push(decoration);
+        }
+      }
+      editor.setDecorations(decorationType, decorations);
+    }
+  }
+}
